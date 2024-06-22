54049f417026c4093428439287031129e5bd37ae
M-Code-H <wessamsarra@gmail.com>
2024-06-22 11:05:48 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
-import "insex.css";
+import "index.css";
 import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
